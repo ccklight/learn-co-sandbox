a011eef69800b0b047318b4e53851651351b7120
Cheryl K <cherylkelson@icloud.com>
2019-07-05 21:37:53 +0000

    Automatically backed up by Learn

---
+++ college-bound/lib/college/bound.rb
@@ -0,0 +1,8 @@
+require "college/bound/version"
+
+module College
+  module Bound
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ college-bound/lib/college/bound/version.rb
@@ -0,0 +1,5 @@
+module College
+  module Bound
+    VERSION = "0.1.0"
+  end
+end
